6f98ab65a895ad0e1d28705e9a19c4a121133d13
Josh Melgar <josh.melgar@gmail.com>
2022-01-22 08:41:23 -0500

    Tab bar added

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBAction func loginButtonPressed(_ sender: Any)
+    {
+        self.performSegue(withIdentifier: "TabBarShow", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
