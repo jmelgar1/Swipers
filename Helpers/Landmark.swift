a1a6c29dadf1d31fb46b65084cdc596215132f6d
Josh Melgar <josh.melgar@gmail.com>
2022-11-22 14:11:51 -0500

    Updated some segues and back buttons

---
+++ Helpers/Landmark.swift
@@ -0,0 +1,8 @@
+//
+//  Landmark.swift
+//  Swipers
+//
+//  Created by Josh Melgar on 11/9/22.
+//
+
+import Foundation
---
+++ Helpers/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  Swipers
+//
+//  Created by Josh Melgar on 11/9/22.
+//
+
+import Foundation
